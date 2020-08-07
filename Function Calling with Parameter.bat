0cb2bc48ea56f413c65604f69bdad9c288c800c6
Mayank Khurmai <mayankkhurmai8@gmail.com>
2020-08-08 01:04:16 +0530

    Create Function Calling with Parameter.bat

---
+++ Function Calling with Parameter.bat
@@ -0,0 +1,10 @@
+@echo off
+:: Line 5 is used to seprate main progrm with the function
+Rem Function is just defined just like the label
+SETLOCAL
+CALL :Display 5 , 10
+EXIT /B %ERRORLEVEL%
+:Display
+echo The value of parameter 1 is %~1
+echo The value of parameter 2 is %~2
+EXIT /B 0
