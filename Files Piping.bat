8a6c7cdb1f92038a1186d60960d48fc9e2520946
Mayank Khurmai <mayankkhurmai8@gmail.com>
2020-08-23 16:53:14 +0530

    Update Files Piping.bat

--- Files Piping.bat
+++ Files Piping.bat
@@ -1,4 +1,4 @@
 @echo off 
-:: Show all the task with notepad and save it in the tasklist.txt file
+:: Show all the task in notepad and save it in the tasklist.txt file
 tasklist | find "notepad" > tasklist.txt
 pause
